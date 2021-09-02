ae925e15e1f717ece698d1ea18fbadd5ac208806
Alexander Aranda <Alexandermarkets1@gmail.com>
2021-09-02 01:06:38 -0700

    Component: Gallery: index.js edit

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,10 +1,22 @@
-import React from 'react'
+import React from 'react';
+import { capitalizeFirstLetter } from '../../utils/helpers';
+import photo from "../../assets/small/commercial/0.jpg";
 
-function Gallery () {
+
+function Gallery (props) {
+    const currencyCategory = {
+        name: "commercial",
+        description: 
+        "Photos of grocery stores, food trucks, and other commercial projects",
+    };
     return (
         <section>
-            <h1>NAME</h1>
-            <p>DESCRIPTION</p>
+            <h1>{capitalizeFirstLetter(currencyCategory.name)}</h1>
+            <p>{currencyCategory.name}</p>
+            <div>
+                <img src={photo}
+                />
+            </div>
         </section>
     )
 }
