f23f7d126a935385ac8dd28651a0d0a68cb19b20
Alexander Aranda <Alexandermarkets1@gmail.com>
2021-09-03 00:31:00 -0700

    components: Contact: __test__:  index.test.js d1st test Describe

--- src/components/Contact/__tests__/index.test.js
+++ src/components/Contact/__tests__/index.test.js
@@ -5,3 +5,15 @@ import '@testing-library/jest-dom/extend-expect'
 import Contact from './Contact'
 
 afterEach(cleanup)
+
+describe('Contact component renders', () => {
+    it('renders', () => {
+    render(<Contact />);
+  });
+  
+    it('renders', () => {
+      const { asFragment } = render(<Contact />)
+      expect(asFragment()).toMatchSnapshot()
+    })
+  })
+  
