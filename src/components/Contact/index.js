651aa5ddf77157bb86718b5ebad9caaa825e7774
Alexander Aranda <Alexandermarkets1@gmail.com>
2021-09-03 00:27:23 -0700

    components: Contact: index.js created{

---
+++ src/components/Contact/index.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+
+import { validateEmail } from '../../utils/helpers';
+
+function ContactForm() {
+  const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+
+  const [errorMessage, setErrorMessage] = useState('');
+  const { name, email, message } = formState;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!errorMessage) {
+      setFormState({ [e.target.name]: e.target.value });
+      console.log('Form', formState);
+    }
+  }
+  return (
+      <section></section>
+    )
+}
+
+export default ContactForm;
